0ca7b83b6c5df7d76b5bdcc4bea70518aec991ce
PedroMajewski <pedrohmssilva@gmail.com>
2024-06-05 15:24:07 -0300

    commit-Header

--- Home/header.js
+++ Home/header.js
@@ -3,4 +3,20 @@ let menu_lateral = document.querySelector(".menu-mobile")
 
 btn_icone.addEventListener("click",function abrir(){
     menu_lateral.classList.toggle("aberto");
+    if (btn_icone.src.includes("menu-mobile.svg")) {
+        btn_icone.src = "./Assets/Header/cross.svg";
+    } else {
+        btn_icone.src = "./Assets/Header/menu-mobile.svg";
+    }
 });
+
+document.addEventListener("click", fecharMenu);
+
+function fecharMenu(event) {
+    if (!menu_lateral.contains(event.target) && !btn_icone.contains(event.target)) {
+        menu_lateral.classList.remove("aberto");
+        btn_icone.src = "./Assets/Header/menu-mobile.svg";
+    }
+}
+
+
