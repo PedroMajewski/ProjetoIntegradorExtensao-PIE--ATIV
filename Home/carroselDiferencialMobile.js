911e0bd3cc5abf833d90fd3ddc6026a09905da1e
PedroMajewski <pedrohmssilva@gmail.com>
2024-06-20 00:54:20 -0300

    commit ajustes

--- Home/carroselDiferencialMobile.js
+++ Home/carroselDiferencialMobile.js
@@ -28,11 +28,12 @@ function carouselMobileDiferencial() {
         function handleTouchStart(event) {
             startX = event.touches[0].clientX;
             container.style.transition = 'none';
+            event.preventDefault();
             
         }
 
         function handleTouchMove(event) {
-            
+            event.preventDefault();
         }
 
         function handleTouchEnd(event) {
