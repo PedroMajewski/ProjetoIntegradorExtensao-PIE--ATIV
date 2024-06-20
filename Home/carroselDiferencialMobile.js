2cfbdb5ada052dab53039e48e9845ed19beb3a73
PedroMajewski <pedrohmssilva@gmail.com>
2024-06-19 23:57:34 -0300

    commit responsividade

--- Home/carroselDiferencialMobile.js
+++ Home/carroselDiferencialMobile.js
@@ -1,34 +1,33 @@
 // Carrossel Versão MOBILE
-
 function carouselMobileDiferencial() {
     if (window.innerWidth <= 900) {
-        const centroDif = document.getElementById("diferencial2");
-        const esquerdaDif = document.getElementById("diferencial1");
-        const direitaDif = document.getElementById("diferencial3");
-        const elementosDif = Array.from(document.querySelectorAll(".div-carroselDiferencial > div"));
-        const containerDif = document.getElementById("div-carroselDiferencial");
-        const dotsDif = document.querySelectorAll(".dotD");
+        const centro = document.getElementById("diferencial2");
+        const esquerda = document.getElementById("diferencial1");
+        const direita = document.getElementById("diferencial3");
+        const elementos = Array.from(document.querySelectorAll(".div-carroselDiferencial > div"));
+        const container = document.getElementById("carroselDif");
+        const dots = document.querySelectorAll(".dotD");
 
         let startX;
         let index = 1; // Índice do card central
 
-        containerDif.addEventListener('touchstart', handleTouchStart, false);
-        containerDif.addEventListener('touchmove', handleTouchMove, false);
-        containerDif.addEventListener('touchend', handleTouchEnd, false);
+        container.addEventListener('touchstart', handleTouchStart, false);
+        container.addEventListener('touchmove', handleTouchMove, false);
+        container.addEventListener('touchend', handleTouchEnd, false);
 
-        dotsDif.forEach((dot, i) => {
+        dots.forEach((dot, i) => {
             dot.addEventListener('click', () => {
                 goToSlide(i); 
             });//Evento de clique em cada DOT :)
         });
 
-        containerDif.addEventListener('touchstart', function(event) {
+        container.addEventListener('touchstart', function(event) {
             event.stopPropagation();
         });
 
         function handleTouchStart(event) {
             startX = event.touches[0].clientX;
-            containerDif.style.transition = 'none';
+            container.style.transition = 'none';
             
         }
 
@@ -45,23 +44,23 @@ function carouselMobileDiferencial() {
             } else if (diffX < -50) {
                 nextSlide();
             } else {
-                containerDif.style.transition = 'transform 0.5s ease-in-out';
-                containerDif.style.transform = `translateX(${startOffset}px)`;
+                container.style.transition = 'transform 0.5s ease-in-out';
+                container.style.transform = `translateX(${startOffset}px)`;
             }
-            containerDif.style.transition = 'transform 0.5s ease-in-out';
+            container.style.transition = 'transform 0.5s ease-in-out';
             event.preventDefault();
         }
 
         function prevSlide() {
-            containerDif.insertBefore(elementosDif[elementosDif.length - 1], elementosDif[0]);
-            elementosDif.unshift(elementosDif.pop());
+            container.insertBefore(elementos[elementos.length - 1], elementos[0]);
+            elementos.unshift(elementos.pop());
             updateSlides();
             updateDotHighlight();
         }
 
         function nextSlide() {
-            containerDif.appendChild(elementosDif[0]);
-            elementosDif.push(elementosDif.shift());
+            container.appendChild(elementos[0]);
+            elementos.push(elementos.shift());
             updateSlides();
             updateDotHighlight();
         }
@@ -82,7 +81,7 @@ function carouselMobileDiferencial() {
         }
 
         function updateSlides() {
-            elementosDif.forEach((el, index) => {
+            elementos.forEach((el, index) => {
                 const distanceFromCenter = index - 1; 
                 const scaleFactor = Math.pow(0.8, Math.abs(distanceFromCenter)); 
                 const opacityFactor = Math.pow(0.7, Math.abs(distanceFromCenter));
@@ -94,17 +93,17 @@ function carouselMobileDiferencial() {
 
         function updateDotHighlight() {
             
-            dotsDif.forEach(dot => {
+            dots.forEach(dot => {
                 dot.classList.remove('highlight');
             });
 
             //Faz com que o DOT respectivo esteja com a classe "highlight"
-            if (elementosDif[1] === centroDif) {
-                dotsDif[1].classList.add('highlight');
-            } else if (elementosDif[1] === esquerdaDif) {
-                dotsDif[0].classList.add('highlight');
-            } else if (elementosDif[1] === direitaDif) {
-                dotsDif[2].classList.add('highlight');
+            if (elementos[1] === centro) {
+                dots[1].classList.add('highlight');
+            } else if (elementos[1] === esquerda) {
+                dots[0].classList.add('highlight');
+            } else if (elementos[1] === direita) {
+                dots[2].classList.add('highlight');
             }
         }
 
