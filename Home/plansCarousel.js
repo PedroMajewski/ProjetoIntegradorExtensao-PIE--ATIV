90623afc07d16a3a9cf842d7ab288256f6d0d376
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2024-06-23 16:28:19 -0300

    commit responsiv celulares

--- Home/plansCarousel.js
+++ Home/plansCarousel.js
@@ -1,5 +1,5 @@
 function carouselMobile() {
-    if (window.innerWidth <= 700) {
+    if (window.innerWidth <= 780) {
         const centro = document.getElementById("plano-2");
         const esquerda = document.getElementById("plano-1");
         const direita = document.getElementById("plano-3");
